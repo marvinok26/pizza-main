1e355d2a829a69f8a3054d32bca3caafe87a5455
Marvin <okongomarvin971@gmail.com>
2024-04-19 15:00:04 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -19,10 +19,102 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
+
+
+class Restaurants(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(rules=("-restaurant_pizzas",)) for restaurant in Restaurant.query.all()]
+        return make_response(restaurants, 200)
+    
+api.add_resource(Restaurants, '/restaurants')
+
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter(Restaurant.id==id).first()
+
+        if restaurant == None:
+            response = make_response(
+                {"error": "Restaurant not found"},
+                404
+            )
+            return response
+    
+        else:
+            return make_response(
+                restaurant.to_dict(),
+                200,
+            )
+        
+    def delete(self, id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+
+        if restaurant == None:
+            return make_response(
+                {"error": "Restaurant not found"},
+                404
+            )
+        
+        else:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return make_response(
+                "",
+                204,
+            )
+
+api.add_resource(RestaurantByID, '/restaurants/<int:id>') 
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(rules=("-restaurant_pizzas",)) for pizza in Pizza.query.all()]
+        return make_response(pizzas, 200)
+
+api.add_resource(Pizzas, '/pizzas')
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+
+        price = data.get("price")
+        pizza_id = data.get("pizza_id")
+        restaurant_id = data.get("restaurant_id")
+
+        if not all([price, pizza_id, restaurant_id]):
+            return make_response(
+                {"errors": ["validation errors"]},
+                400,
+            )
+        
+        if price < 1 or price > 30:
+            return make_response(
+                {"errors": ["validation errors"]},
+                400,
+            )
+        
+        pizza = Pizza.query.filter(Pizza.id == pizza_id).first()
+        restaurant = Restaurant.query.filter(Restaurant.id == restaurant_id).first()
+
+        if pizza is None or restaurant is None:
+            return make_response(
+                {"errors": ["validation errors"]},
+                400,
+            )
+        
+        restaurant_pizza = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+
+        return make_response(
+            restaurant_pizza.to_dict(),
+            201,
+        )
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 
 if __name__ == '__main__':
